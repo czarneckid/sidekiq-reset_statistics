8ebf1692407f28a471a3decfb536c1edafc33ac7
David Czarnecki <me@davidczarnecki.com>
2014-01-12 15:44:15 -0800

    Version bump to 1.1.0 and updated CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.1.0 (2014-01-12)
+
+* Use updated `Sidekiq::Stats#reset` to reset individual stats independently. Thanks @bwthomas
+
 ## 1.0.0 (2013-09-12)
 
 * Initial release
--- spec/sidekiq-reset_statistics/version_spec.rb
+++ spec/sidekiq-reset_statistics/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Sidekiq::ResetStatistics::VERSION' do
   it 'should be the correct version' do
-    Sidekiq::ResetStatistics::VERSION.should == '1.0.0'
+    Sidekiq::ResetStatistics::VERSION.should == '1.1.0'
   end
 end
